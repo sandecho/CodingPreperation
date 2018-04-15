b848c11068e0715da993cfffea768f6ef442834f
sandecho <tssandeepkumarchoudhary@gmail.com>
2018-04-15 23:18:12 +0530

    Modified

--- Array/ArrayProblem1.java
+++ Array/ArrayProblem1.java
@@ -1,9 +1,9 @@
-public class ArrayProblem1{
+public class ArrayProblem1 {
 
-public static void main (String args[]){
+    public static void main (String args[]) {
 
-System.out.println("Hello Git. Welcome to DataStructure");
-}
+        System.out.println("Hello Git. Welcome to DataStructure");
+    }
 
 }
 
